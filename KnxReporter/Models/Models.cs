9fab23f65a90b0de051cd60a9aefa69e2fcd6740
matik251 <mateusz.pawel.kula@gmail.com>
2021-01-03 17:31:32 +0100

    WorkingDG

---
+++ KnxReporter/Models/Models.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KnxReporter.Models
+{
+    public class Telegram
+    {
+        public long TID { get; set; }
+
+        public string TimestampS { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public string Service { get; set; }
+        public string FrameFormat { get; set; }
+
+        public string RawData { get; set; }
+    }
+
+    public enum KnxServices
+    {
+        L_Busmon_ind
+    }
+
+    public class KnxDecoderWorkers
+    {
+        public int Id { get; set; }
+        public int IsBusy { get; set; }
+    }
+}
+
